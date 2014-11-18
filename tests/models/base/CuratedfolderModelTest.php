976d95d3ceee4fe0623feadf5d592035fbc5189e
mgrauer <michael.grauer@kitware.com>
2014-11-18 18:56:08 +0000

    whitespace fixes

--- tests/models/base/CuratedfolderModelTest.php
+++ tests/models/base/CuratedfolderModelTest.php
@@ -12,7 +12,8 @@
          http://www.apache.org/licenses/LICENSE-2.0.txt
 
  Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  See the License for the specific language governing permissions and
  limitations under the License.
 =========================================================================*/
@@ -51,7 +52,7 @@ class CuratedfolderModelTest extends DatabaseTestCase
 
     // curate a folder and ensure it is tracked for curation
     $folderDao = $this->Folder->load(1000);
-$disabled = $curatedfolderModel->disableFolderCuration($folderDao);
+    $disabled = $curatedfolderModel->disableFolderCuration($folderDao);
     $curatedfolderDao = $curatedfolderModel->enableFolderCuration($folderDao);
 
     $this->assertEquals($curatedfolderDao->getFolderId(), $folderDao->getFolderId());
